3cc86d881b7dc033bb8de40f5fb8374ec02bbd2d
anhk <anhk_fei@163.com>
2020-11-22 23:12:08 +0800

    格式化代码

--- src/main/java/com/anhk/modules/oss/controller/SysOssController.java
+++ src/main/java/com/anhk/modules/oss/controller/SysOssController.java
@@ -35,23 +35,23 @@ import java.util.Map;
 @RestController
 @RequestMapping("sys/oss")
 public class SysOssController {
-	@Autowired
-	private SysOssService sysOssService;
+    @Autowired
+    private SysOssService sysOssService;
     @Autowired
     private SysConfigService sysConfigService;
 
     private final static String KEY = ConfigConstant.CLOUD_STORAGE_CONFIG_KEY;
-	
-	/**
-	 * 列表
-	 */
-	@RequestMapping("/list")
-	@RequiresPermissions("sys:oss:all")
-	public ResultVo list(@RequestParam Map<String, Object> params){
-		PageUtils page = sysOssService.queryPage(params);
 
-		return ResultVo.ok().put("page", page);
-	}
+    /**
+     * 列表
+     */
+    @RequestMapping("/list")
+    @RequiresPermissions("sys:oss:all")
+    public ResultVo list(@RequestParam Map<String, Object> params) {
+        PageUtils page = sysOssService.queryPage(params);
+
+        return ResultVo.ok().put("page", page);
+    }
 
 
     /**
@@ -59,72 +59,72 @@ public class SysOssController {
      */
     @RequestMapping("/config")
     @RequiresPermissions("sys:oss:all")
-    public ResultVo config(){
+    public ResultVo config() {
         CloudStorageConfig config = sysConfigService.getConfigObject(KEY, CloudStorageConfig.class);
 
         return ResultVo.ok().put("config", config);
     }
 
 
-	/**
-	 * 保存云存储配置信息
-	 */
-	@RequestMapping("/saveConfig")
-	@RequiresPermissions("sys:oss:all")
-	public ResultVo saveConfig(@RequestBody CloudStorageConfig config){
-		//校验类型
-		ValidatorUtils.validateEntity(config);
+    /**
+     * 保存云存储配置信息
+     */
+    @RequestMapping("/saveConfig")
+    @RequiresPermissions("sys:oss:all")
+    public ResultVo saveConfig(@RequestBody CloudStorageConfig config) {
+        //校验类型
+        ValidatorUtils.validateEntity(config);
 
-		if(config.getType() == Constant.CloudService.QINIU.getValue()){
-			//校验七牛数据
-			ValidatorUtils.validateEntity(config, QiniuGroup.class);
-		}else if(config.getType() == Constant.CloudService.ALIYUN.getValue()){
-			//校验阿里云数据
-			ValidatorUtils.validateEntity(config, AliyunGroup.class);
-		}else if(config.getType() == Constant.CloudService.QCLOUD.getValue()){
-			//校验腾讯云数据
-			ValidatorUtils.validateEntity(config, QcloudGroup.class);
-		}
+        if (config.getType() == Constant.CloudService.QINIU.getValue()) {
+            //校验七牛数据
+            ValidatorUtils.validateEntity(config, QiniuGroup.class);
+        } else if (config.getType() == Constant.CloudService.ALIYUN.getValue()) {
+            //校验阿里云数据
+            ValidatorUtils.validateEntity(config, AliyunGroup.class);
+        } else if (config.getType() == Constant.CloudService.QCLOUD.getValue()) {
+            //校验腾讯云数据
+            ValidatorUtils.validateEntity(config, QcloudGroup.class);
+        }
 
         sysConfigService.updateValueByKey(KEY, new Gson().toJson(config));
 
-		return ResultVo.ok();
-	}
-	
-
-	/**
-	 * 上传文件
-	 */
-	@RequestMapping("/upload")
-	@RequiresPermissions("sys:oss:all")
-	public ResultVo upload(@RequestParam("file") MultipartFile file) throws Exception {
-		if (file.isEmpty()) {
-			throw new BusinessException("上传文件不能为空");
-		}
-
-		//上传文件
-		String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
-		String url = OSSFactory.build().uploadSuffix(file.getBytes(), suffix);
-
-		//保存文件信息
-		SysOssEntity ossEntity = new SysOssEntity();
-		ossEntity.setUrl(url);
-		ossEntity.setCreateDate(new Date());
-		sysOssService.save(ossEntity);
-
-		return ResultVo.ok().put("url", url);
-	}
+        return ResultVo.ok();
+    }
 
 
-	/**
-	 * 删除
-	 */
-	@RequestMapping("/delete")
-	@RequiresPermissions("sys:oss:all")
-	public ResultVo delete(@RequestBody Long[] ids){
-		sysOssService.removeByIds(Arrays.asList(ids));
+    /**
+     * 上传文件
+     */
+    @RequestMapping("/upload")
+    @RequiresPermissions("sys:oss:all")
+    public ResultVo upload(@RequestParam("file") MultipartFile file) throws Exception {
+        if (file.isEmpty()) {
+            throw new BusinessException("上传文件不能为空");
+        }
 
-		return ResultVo.ok();
-	}
+        //上传文件
+        String suffix = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        String url = OSSFactory.build().uploadSuffix(file.getBytes(), suffix);
+
+        //保存文件信息
+        SysOssEntity ossEntity = new SysOssEntity();
+        ossEntity.setUrl(url);
+        ossEntity.setCreateDate(new Date());
+        sysOssService.save(ossEntity);
+
+        return ResultVo.ok().put("url", url);
+    }
+
+
+    /**
+     * 删除
+     */
+    @RequestMapping("/delete")
+    @RequiresPermissions("sys:oss:all")
+    public ResultVo delete(@RequestBody Long[] ids) {
+        sysOssService.removeByIds(Arrays.asList(ids));
+
+        return ResultVo.ok();
+    }
 
 }
